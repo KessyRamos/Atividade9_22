65c78445e6a38de8778f30a7a7aa8fe2b68e88a2
KessyRamos <kessyellenramos@gmail.com>
2022-06-05 09:59:01 -0300

    PrimeiroCommit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿double n1 = 10;
+double n2 = 15;
+double resto;
+
+Console.WriteLine("Calcular o resto da divisão!!!");
+
+resto = n1 % n2;
+
+if (n2 == 0){
+    Console.WriteLine("Não é possível realizar divisão por 0!");
+}else{
+    Console.Write($"O resto da divisão entre {n1} e {n2} é {resto}");
+}
+
